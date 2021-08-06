b307eea6a1668acf2a71a5fa14f4faa3cb5bea99
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-08-06 00:00:25 -0400

    Vercellable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react'
 
 const Home: FC = () => {
-  return <div />
+  return (
+    <>
+      <h1> Guess Whomst </h1>
+      <h2> A Game Of Subjective Judgements </h2>
+
+      <button> JOIN </button>
+    </>
+  )
 }
 
 export default Home
