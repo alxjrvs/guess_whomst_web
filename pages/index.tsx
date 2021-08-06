6a17eed519d47972fd1c857368d6d91a5d79b2db
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-08-06 00:07:09 -0400

    Whiff of style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import { FC } from 'react'
 
 const Home: FC = () => {
   return (
-    <>
+    <section className="text-center">
       <h1> Guess Whomst </h1>
       <h2> A Game Of Subjective Judgements </h2>
 
       <button> JOIN </button>
-    </>
+    </section>
   )
 }
 
